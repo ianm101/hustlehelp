ac30917ae705d292903a268c2ca8817a307ebdd4
Ian Murray <im6@princeton.edu>
2023-04-19 13:35:00 -0400

    message checking testing

--- check_pinterest_messages.py
+++ check_pinterest_messages.py
@@ -11,6 +11,12 @@ def check_messages(driver):
 
     msgs_container = driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div/ul")
     msgs_elements = msgs_container.find_elements(By.XPATH, "./child::*")
+
+    # Click first message (testing)
+    msgs_elements[0].click()
     
-    print(len(msgs_elements))
+    # Should be able to identify all messages (html elements) via this class
+    individual_message_messages = driver.find_elements(By.CLASS_NAME, "Jea zI7 iyn Hsu")
+    print(f'Individual Messages: {len(individual_message_messages)}')
+    
     print("done")
